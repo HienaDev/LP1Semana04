2b043442fec202d4328a110199176261337ea00f
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-22 14:43:28 +0000

    Added Lucas function with input 7

---
+++ Lucas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine(Lucas(7));
+        }
+
+        private static int Lucas(int n)
+        {
+            if (n == 0)
+                return 2;
+            else if (n == 1)
+                return 1;
+            else
+                return (Lucas(n - 2) + Lucas(n - 1));
+        }
+    }
+}
