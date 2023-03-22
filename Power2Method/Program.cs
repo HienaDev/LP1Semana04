0712ded2e3ab036c0199428b80ffb6e3367bf9ce
HienaDev <ant.rodrigues1999@gmail.com>
2023-03-22 14:23:10 +0000

    Added xml comment to main

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,10 +4,14 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// Displays the power of 2 until 5, 7 and 10.
+        /// </summary>
+        /// <param name="args"> Arguments received from the terminal </param>
         static void Main(string[] args)
         {
             PowerOf2Until5();
-            
+
             PowerOf2UntilN(7);
             PowerOf2UntilN(10);
         }
